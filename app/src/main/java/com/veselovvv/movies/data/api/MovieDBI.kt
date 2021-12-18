5acc446fcbec642bd75c31126ac6f9ac4cabc819
veselovvv <vycheslav5veselov@gmail.com>
2021-12-18 10:29:09 +0200

    Added layout files

--- app/src/main/java/com/veselovvv/movies/data/api/MovieDBI.kt
+++ app/src/main/java/com/veselovvv/movies/data/api/MovieDBI.kt
@@ -1,12 +1,17 @@
 package com.veselovvv.movies.data.api
 
 import com.veselovvv.movies.data.MovieDetails
+import com.veselovvv.movies.data.MovieResponse
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface MovieDBI {
 
     @GET("movie/{movie_id}")
     fun getMovieDetails(@Path("movie_id") id: Int): Single<MovieDetails>
+
+    @GET("movie/popular")
+    fun getPopularMovie(@Query("page") page: Int): Single<MovieResponse>
 }
