24ccdd12d3aeadac4cc173dc5d5f785c464361f4
veselovvv <vycheslav5veselov@gmail.com>
2021-12-20 15:19:18 +0200

    Added logic for MainActivity and fixed UI

--- app/src/main/java/com/veselovvv/movies/ui/MainActivity.kt
+++ app/src/main/java/com/veselovvv/movies/ui/MainActivity.kt
@@ -1,25 +1,77 @@
 package com.veselovvv.movies.ui
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.button.MaterialButton
+import android.view.View
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.textview.MaterialTextView
 import com.veselovvv.movies.R
+import com.veselovvv.movies.data.api.MovieDBClient
+import com.veselovvv.movies.data.api.MovieDBI
+import com.veselovvv.movies.data.repositories.NetworkState
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var button: MaterialButton
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var errorTextView: MaterialTextView
+
+    lateinit var movieRepository: MoviePagedListRepository
+    private lateinit var viewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button = findViewById(R.id.button)
+        recyclerView = findViewById(R.id.movie_list)
+        progressBar = findViewById(R.id.progress_bar_popular)
+        errorTextView = findViewById(R.id.error_popular)
 
-        button.setOnClickListener {
-            val intent = Intent(this, MovieActivity::class.java)
-            intent.putExtra("id",299534)
-            this.startActivity(intent)
+        val apiService: MovieDBI = MovieDBClient.getClient()
+        movieRepository = MoviePagedListRepository(apiService)
+        viewModel = getViewModel()
+
+        val movieAdapter = MoviePagedListAdapter(this)
+        val gridLayoutManager = GridLayoutManager(this, 2)
+
+        gridLayoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                val viewType = movieAdapter.getItemViewType(position)
+                // MOVIE_VIEW_TYPE needs 1 out of 2 span, NETWORK_VIEW_TYPE - all 2 span:
+                if (viewType == movieAdapter.MOVIE_VIEW_TYPE) return 1 else return 2
+            }
         }
+
+        recyclerView.apply {
+            layoutManager = gridLayoutManager
+            setHasFixedSize(true)
+            adapter = movieAdapter
+        }
+
+        viewModel.moviePagedList.observe(this, Observer { movieAdapter.submitList(it) })
+        viewModel.networkState.observe(this, Observer {
+            progressBar.visibility =
+                if (viewModel.listIsEmpty() && it == NetworkState.LOADING) View.VISIBLE else View.GONE
+
+            errorTextView.visibility =
+                if (viewModel.listIsEmpty() && it == NetworkState.ERROR) View.VISIBLE else View.GONE
+
+            if (!viewModel.listIsEmpty()) movieAdapter.setNetworkState(it)
+        })
+    }
+
+    private fun getViewModel(): MainActivityViewModel {
+        return ViewModelProviders.of(this, object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                @Suppress("UNCHECKED_CAST")
+                return MainActivityViewModel(movieRepository) as T
+            }
+        })[MainActivityViewModel::class.java]
     }
 }
